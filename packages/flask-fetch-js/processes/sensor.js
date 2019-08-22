a891778ca218899f5b0329dd6c50fa94acb629f2
Franke <cyhsu@om702a-ednmp.ad.geos.tamu.edu>
2019-08-22 17:15:54 -0500

    Don't need packages anymore

--- packages/flask-fetch-js/processes/sensor.js
+++
@@ -1,8 +0,0 @@
-// sensor.js
-
-function reportReading() {
-    const temperature = (Math.random() * 20) - 5; // Range of -5 to 15
-    process.stdout.write(temperature + '\n'); // Write with newline char
-    setTimeout(reportReading, Math.random() * 5000); // Wait 0 to 5 seconds
-}
-reportReading();
--- packages/flask-fetch-js/test/abc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl --header "Content-Type: application/json" --request POST --data '{"name":"Julian","message":"Posting JSON data to Flask!"}' http://127.0.0.1:5000/json
-
