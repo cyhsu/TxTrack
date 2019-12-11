7e84b8ba4003a7ccd41f8c86730aadfe9f77a721
C.Y. Hsu <chsu1@tamu.edu>
2019-12-10 19:13:18 -0600

    Create run_docker.sh

---
+++ docker/run_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#--- USER INTERFACE
+local_port=5000
+local_path=$PWD
+background=True
+
+tar xvf txtrack-flask.tar
+
+if [ "$background" == True ]; then 
+	docker run -d -p ${local_port}:8012 --volume=${local_path} txtrack-flask
+else
+	docker run -p ${local_port}:8012 --volume=${local_path} txtrack-flask
+fi
