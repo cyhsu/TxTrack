74047c4ec94e2b662861ab72006d2a6e446b511e
C.Y. Hsu <chsu1@tamu.edu>
2019-12-11 14:11:13 -0600

    Update build_docker.sh
    #-- Bug: docker build fixed.
    #-- Bug: login-mode permission fixed.

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -14,12 +14,12 @@ mv ./leaflet.timedimention.trajectory/* .
 rmdir leaflet.timedimention.trajectory
 
 #-- Build my private Docker Image
-docker build -t txtrack-flask 
+docker build -t txtrack-flask .
 
 #-- Run Docker Image
 if [ "$background" == True ]; then 
-	docker run -d -p ${local_port}:8012 --volume=${local_path} txtrack-flask
+	docker run -d -p ${local_port}:8012 --volume=${local_path} txtrack-flask:latest
 else
-	docker run -p ${local_port}:8012 --volume=${local_path} txtrack-flask
+	docker run -p ${local_port}:8012 --volume=${local_path} txtrack-flask:latest
 fi
 
