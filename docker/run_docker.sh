e771eea4417d7d2ecac30d66f858ad2c95eb4a73
C.Y. Hsu <chsu1@tamu.edu>
2019-12-11 14:09:42 -0600

    Update run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -8,7 +8,7 @@ background=True
 tar xvf txtrack-flask.tar
 
 if [ "$background" == True ]; then 
-	docker run -d -p ${local_port}:8012 --volume=${local_path} txtrack-flask
+	docker run -d -p ${local_port}:8012 --volume=${local_path} txtrack-flask:latest
 else
-	docker run -p ${local_port}:8012 --volume=${local_path} txtrack-flask
+	docker run -p ${local_port}:8012 --volume=${local_path} txtrack-flask:latest
 fi
