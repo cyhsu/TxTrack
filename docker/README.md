0db71506a5aa1c015c6d75a884377ebb220800e5
C.Y. Hsu <chsu1@tamu.edu>
2019-12-10 19:14:14 -0600

    Add files via upload

---
+++ docker/README.md
@@ -0,0 +1,14 @@
+# Particle Trajectories using High Frequency Radar
+
+#### Project Structure
+
+	> {'Project short name': 'ParTHdar',
+	>  'Funding Source': 'Texas General Land Office', 
+	>  'Project PI': 'Steve DiMarco',
+	> 	'Project co-PI': 'Kerri Whilden',
+	>  'Web Creator': 'Chuan-Yuan Hsu',
+	>	'Institute': 'Geochemical and Environmental Research Group - Texas A&M University; Gulf of Mexico Ocean Observing System'}
+
+
+
+docker run -p 5000:8012 --volume=/Users/cyhsu/dev/TxTrack/dockerized/a2:/home/TxTrack txtrack-flask
---
+++ docker/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#--- USER INTERFACE
+local_port=5000
+local_path=$PWD
+background=True
+
+
+#-- Download TxTrack-flask package. 
+git clone https://github.com/cyhsu/leaflet.timedimention.trajectory.git
+
+#-- release directory: leaflet.timedimention.trajectory
+mv ./leaflet.timedimention.trajectory/* .
+rmdir leaflet.timedimention.trajectory
+
+#-- Build my private Docker Image
+docker build -t txtrack-flask 
+
+#-- Run Docker Image
+if [ "$background" == True ]; then 
+	docker run -d -p ${local_port}:8012 --volume=${local_path} txtrack-flask
+else
+	docker run -p ${local_port}:8012 --volume=${local_path} txtrack-flask
+fi
+
